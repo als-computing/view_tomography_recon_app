1f47634f5473489cc6005050a4d8331f1a3aa658
seijdeleon <seijdeleon@lbl.gov>
2026-06-15 09:04:03 -0700

    install newest tiled with oidcRedirectUrl

--- src/Header.tsx
+++ src/Header.tsx
@@ -36,7 +36,7 @@ export const Header = ({ logoUrl, title, fileName, onSelect }: HeaderProps) => {
           <div className="header-file-name">{fileName}</div>
         </div>
         <div>
-                    <Tiled isButtonMode={true} onSelectCallback={handleTiledWidgetSelect} tiledBaseUrl={getTiledBaseUrl()} initialPath='beamlines/bl832/processed/example_samples' singleColumnMode={true} includeAuthTokensInSelectCallback={true}/>
+          <Tiled oidcRedirectUrl='http://tiled-test:5174/react/' isButtonMode={true} onSelectCallback={handleTiledWidgetSelect} tiledBaseUrl={getTiledBaseUrl()} singleColumnMode={true} includeAuthTokensInSelectCallback={true}/>
           {/* <Tiled isButtonMode={true} onSelectCallback={handleTiledWidgetSelect} tiledBaseUrl={getTiledBaseUrl()} initialPath='beamlines/bl832/processed/BLS-00761_clark' includeAuthTokensInSelectCallback={true} singleColumnMode={true}/> */}
           {/* <Tiled isButtonMode={true} onSelectCallback={handleTiledWidgetSelect} tiledBaseUrl={getTiledBaseUrl()} includeAuthTokensInSelectCallback={true} singleColumnMode={true}/> */}
 
